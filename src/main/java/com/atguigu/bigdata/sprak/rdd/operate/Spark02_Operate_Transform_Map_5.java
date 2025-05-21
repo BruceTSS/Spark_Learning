5b27bfa64809c88cc031553e5dc8b7a6ab6fa9fa
BA-FD-TZB <tianzb@baiantek.com>
2025-05-21 19:22:52 +0800

    RDD的Map方法学习2

---
+++ src/main/java/com/atguigu/bigdata/sprak/rdd/operate/Spark02_Operate_Transform_Map_5.java
@@ -0,0 +1,45 @@
+package com.atguigu.bigdata.sprak.rdd.operate;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Spark02_Operate_Transform_Map_5 {
+    public static void main(String[] args) {
+        // 添加这行代码设置hadoop.home.dir
+        System.setProperty("hadoop.home.dir", "D:\\software\\hadoop");  // 替换为你的实际路径
+
+        // 添加这行解决Windows下的Hadoop原生库问题
+        System.load("D:\\software\\hadoop\\bin\\hadoop.dll");
+
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("local[2]");
+        sparkConf.setAppName("Spark");
+
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+
+        // TODO RDD方法
+        // 【1,2】  【3,4】
+        JavaRDD<Integer> javaRDD = javaSparkContext.parallelize(
+               nums,2
+        );
+
+        // TODO map方法的作用就是将传入的A转换为B返回，但是没有限制A和B的关系。
+        JavaRDD<Integer> mapRDD = javaRDD.map(
+                num -> {
+                    System.out.println("@" + num);
+                    return num * 2;
+                }
+        );
+
+        mapRDD.collect();
+
+
+        javaSparkContext.close();
+    }
+}
---
+++ src/main/java/com/atguigu/bigdata/sprak/rdd/operate/Spark02_Operate_Transform_Map_6.java
@@ -0,0 +1,52 @@
+package com.atguigu.bigdata.sprak.rdd.operate;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Spark02_Operate_Transform_Map_6 {
+    public static void main(String[] args) {
+        // 添加这行代码设置hadoop.home.dir
+        System.setProperty("hadoop.home.dir", "D:\\software\\hadoop");  // 替换为你的实际路径
+
+        // 添加这行解决Windows下的Hadoop原生库问题
+        System.load("D:\\software\\hadoop\\bin\\hadoop.dll");
+
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("local[2]");
+        sparkConf.setAppName("Spark");
+
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+
+        // TODO RDD方法
+        // 【1,2】  【3,4】
+        JavaRDD<Integer> javaRDD = javaSparkContext.parallelize(
+               nums,2
+        );
+
+        // TODO map方法的作用就是将传入的A转换为B返回，但是没有限制A和B的关系。
+        JavaRDD<Integer> mapRDD1 = javaRDD.map(
+                num -> {
+                    System.out.println("@" + num);
+                    return num;
+                }
+        );
+
+        JavaRDD<Integer> mapRDD2 = mapRDD1.map(
+                num -> {
+                    System.out.println("#" + num);
+                    return num;
+                }
+        );
+
+        mapRDD2.collect();
+
+
+        javaSparkContext.close();
+    }
+}
